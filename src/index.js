61040d0d0db1ae88de74ad4bf31c8310782b836f
Halyna Urbanska <webenot@mail.ua>
2020-03-28 13:13:02 +0200

    Day 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+const movie = {
+  title: 'Avengers: Infinity War',
+  vote_average: 8.5,
+  image: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4d/Avengers_Infinity_War_poster.jpg/204px-Avengers_Infinity_War_poster.jpg',
+  overview: 'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.'
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+class Image extends React.Component {
+  render() {
+    console.log('Image props', this.props);
+    const { src, alt } = this.props;
+    return (
+      <img src={src} alt={alt}/>
+    )
+  }
+}
+
+class MovieItem extends React.Component {
+  render() {
+    console.log('MovieItem props', this.props);
+    const {
+      data: { title, vote_average, image, overview }
+    } = this.props;
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+        <p>{overview}</p>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <MovieItem data={movie}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
